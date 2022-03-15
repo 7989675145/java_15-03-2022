bc2f8a284dbf7ec6f61dd0031c5349ffa98640ff
Hemanth <80045609+7989675145@users.noreply.github.com>
2022-03-15 15:31:51 +0530

    Add files via upload
    Declaring methods on one page and calling them on another page

---
+++ LearningMethods.java
@@ -0,0 +1,8 @@
+package ford;
+
+public class LearningMethods {
+
+	public static void main(String[] args) {
+		System.out.println(MyUtils.diff(2,3));
+	}
+}
---
+++ MyUtils.java
@@ -0,0 +1,19 @@
+package ford;
+
+public class MyUtils {
+	public int ope(int a,int b,int oper) {
+		return a+b;
+	}
+		public static int sum(int a,int b) {
+			return a+b;
+		}
+		public static int diff(int a,int b) {
+			return a-b;
+		}
+		public static int mul(int a,int b) {
+			return a*b;
+		}
+		public static int div(int a,int b) {
+			return a/b;
+		}
+}
